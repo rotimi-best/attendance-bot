cd62bbb3f696159fc05365a1fe80e7946b47704d
Rotimi-Best <rotimiibitoyebest@yahoo.co.uk>
2018-12-11 23:58:53 +0200

    Otherwise controller

--- Controllers/otherwiseController.js
+++ Controllers/otherwiseController.js
@@ -1,14 +1,16 @@
 const { log } = console;
 const { TelegramBaseController } = require("telegram-node-bot");
-
+const HelpController = require("./helpController");
+const helperController = new HelpController();
 
 class OtherwiseController extends TelegramBaseController {
-    handle($) {
-        const msg = $.message.text;
-        const id = $.message.chat.id;
-        log(id)
-        log(msg)
-    }
+  handle($) {
+    const msg = $.message.text;
+    const id = $.message.chat.id;
+    log(id);
+    log(msg);
+    $.sendMessage("I am a robot and didn't quite understand what you said");
+  }
 }
 
 module.exports = OtherwiseController;
