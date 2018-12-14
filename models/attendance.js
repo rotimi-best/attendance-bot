27650b9c9af26f2fbc76b18a4e616045b32c8000
Rotimi-Best <rotimiibitoyebest@yahoo.co.uk>
2018-12-14 10:53:53 +0200

    MOdel update

--- models/attendance.js
+++ models/attendance.js
@@ -1,18 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema();
-const timestamp = require('mongoose-timestamp');
+const timestamp = require("mongoose-timestamp");
 
 const AttendanceSchema = new Schema({
-	groupName: String,
-	ownerTelegramId: Number,
-	result: [{
-		studentName: String,
-		present: Boolean
-	}]
+  groupName: String,
+  ownerTelegramId: Number,
+  result: [
+    {
+      studentName: String,
+      present: Boolean
+    }
+  ]
 });
 
 AttendanceSchema.plugin(timestamp);
 
-const Attendance = mongoose.model('User', AttendanceSchema);
+const Attendance = mongoose.model("User", AttendanceSchema);
 
 module.exports = Attendance;
--- models/user.js
+++ models/user.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema();
-const timestamp = require('mongoose-timestamp');
+const timestamp = require("mongoose-timestamp");
 
 const UserSchema = new Schema({
   name: String,
-  telegramId: Number
+  telegramId: Number,
+  spreadsheetLink: String
 });
 
 UserSchema.plugin(timestamp);
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
