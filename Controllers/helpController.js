9289de2e4755251a6cd757e0ccb1b2aabbd6df4f
Rotimi-Best <rotimiibitoyebest@yahoo.co.uk>
2018-12-23 14:10:13 +0200

    Editted helpController

--- Controllers/helpController.js
+++ Controllers/helpController.js
@@ -3,12 +3,12 @@ const TelegramBaseController = Telegram.TelegramBaseController;
 
 class HelpController extends TelegramBaseController {
   /**
-   * Scope of the message
+   * Listen to /help command
    * @param {Scope} $
    */
   helpHandler($) {
     $.sendMessage(
-      `You can control me by sending these commands:\n\n/newgroup - create a new group (like a class with students)\n/mygroups - take attendance of your group\n/help - get help\n\nEdit Group\n/rename - change a group's name\n/addstudent - add student(s) to a group\n/deletegroup - delete a group\n\nAttendance\n/editattendance - repeat attendance\n/viewallattendance - view register\n\nStill confused? Talk to my [creator](https://t.me/living_logos)`,
+      `You can control me by sending these commands:\n\nGeneral\n/newgroup - create a new group (like a class with students)\n/mygroups - take attendance of your group\n/help - get help\n\nEdit Group\n/rename - change a group's name\n/addstudent - add student(s) to a group\n/deletegroup - delete a group\n\nAttendance\n/editattendance - repeat attendance\n/viewallattendance - view register\n\nStill confused? Talk to my [creator](https://t.me/living_logos)`,
       { parse_mode: "Markdown", disable_web_page_preview: true }
     );
   }
